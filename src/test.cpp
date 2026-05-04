cc7fa4c7d9b0152b01940e264850d96be56e9f7b
Jeff Greer <greergan@gmail.com>
2026-05-03 18:31:08 -0700

    Code cleanup  Changes to be committed: 	modified:   .gitignore 	modified:   required_packages 	modified:   src/test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,11 @@
-#include <slim/common/http/url.h>
 #include <slim/common/log.h>
 
 int main() {
-
+    using namespace slim::common;
+    log::trace(log::Message{__func__, "Begins", __FILE__, __LINE__});
+#ifdef USING_SLIM_VALUE
+#else
+#endif
+    log::trace(log::Message{__func__, "Ends", __FILE__, __LINE__});
     return 0;
 }
