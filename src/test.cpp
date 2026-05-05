d3e8c6ccb8cb94eea2c4e657c724540dd16e0768
Jeff Greer <greergan@gmail.com>
2026-05-04 20:40:32 -0700

    Added simple SlimValue tests
    	modified:   src/test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,16 @@
+#include <format>
 #include <slim/common/log.h>
+#include <slim/SlimValue.hpp>
 
 int main() {
     using namespace slim::common;
     log::trace(log::Message{__func__, "Begins", __FILE__, __LINE__});
-#ifdef USING_SLIM_VALUE
-#else
-#endif
+
+    slim::SlimValue a = 42;
+    log::debug(log::Message{__func__, std::format("a={}", a.to_string()), __FILE__, __LINE__});
+    slim::SlimValue b = "gone with the wind";
+    log::debug(log::Message{__func__, std::format("b={}", b.to_string()), __FILE__, __LINE__});
+
     log::trace(log::Message{__func__, "Ends", __FILE__, __LINE__});
     return 0;
 }
