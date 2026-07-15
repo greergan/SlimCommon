9492b1c8febd188a4ce248b725b68940a6715190
Jeff Greer <greergan@gmail.com>
2026-07-14 22:42:10 -0700

    config: remove src/test.cpp

--- src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <slim/common/log.h>
-
-int main() {
-    using namespace slim::common;
-    log::trace(log::Message{__func__, "Begins", __FILE__, __LINE__});
-
-    log::trace(log::Message{__func__, "Ends", __FILE__, __LINE__});
-    return 0;
-}
